597666548b143faa5035ff44cf72324eca8e2160
Aries <aries@aspirix.se>
2019-10-15 23:59:11 +0200

    Update of package info to publish on Gemfury

--- packages/regression_model/setup.py
+++ packages/regression_model/setup.py
@@ -11,9 +11,9 @@ from setuptools import find_packages, setup
 # Package meta-data.
 NAME = 'regression_model'
 DESCRIPTION = 'Train and deploy regression model.'
-URL = 'your github project'
-EMAIL = 'your_email@email.com'
-AUTHOR = 'Your name'
+URL = 'https://github.com/Loudness/deployML/tree/master/packages/regression_model/regression_model'
+EMAIL = 'aries000@gmail.com'
+AUTHOR = 'Aries'
 REQUIRES_PYTHON = '>=3.6.0'
 
 
